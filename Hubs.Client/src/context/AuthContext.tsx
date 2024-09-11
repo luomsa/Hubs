6c490f68eadbbc467743010ccae843717f042011
luomsa <git@luomsa.com>
2024-09-11 18:55:13 +0300

    setup query client and auth context

---
+++ Hubs.Client/src/context/AuthContext.tsx
@@ -0,0 +1,32 @@
+﻿import {
+  createContext,
+  Dispatch,
+  ReactElement,
+  SetStateAction,
+  useContext,
+  useState,
+} from "react";
+
+const AuthContext = createContext<AuthContext | null>(null);
+type AuthState = {
+  username: string;
+};
+type AuthContext = {
+  user: AuthState | null;
+  setUser: Dispatch<SetStateAction<AuthState | null>>;
+};
+const AuthProvider = ({ children }: { children: ReactElement }) => {
+  const [user, setUser] = useState<AuthState | null>(null);
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context == null)
+    throw new Error("AuthContext has not been initialized yet");
+  return context;
+};
+export default AuthProvider;
--- Hubs.Client/src/main.tsx
+++ Hubs.Client/src/main.tsx
@@ -1,13 +1,20 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import {ChakraProvider} from "@chakra-ui/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import AuthProvider from "./context/AuthContext.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <ChakraProvider>
-            <App/>
-        </ChakraProvider>
-    </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryClientProvider>
+    </ChakraProvider>
+  </StrictMode>,
+);
