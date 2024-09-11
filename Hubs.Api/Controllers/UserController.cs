ce02a478ab7f25bd07a9b61e1b347917cee51fc7
luomsa <git@luomsa.com>
2024-09-11 18:59:46 +0300

    setup user controller

---
+++ Hubs.Api/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Hubs.Api.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hubs.Api.Controllers;
+[ApiController]
+[Route("users")]
+public class UserController : ControllerBase
+{
+    private readonly SignInManager<User> _signInManager;
+
+    public UserController(SignInManager<User> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+
+    [Authorize]
+    [Route("me")]
+    [HttpGet]
+    public async Task<IResult> GetCurrentUser()
+    {
+        var user = await _signInManager.UserManager.GetUserAsync(HttpContext.User);
+        if (user is null)
+        {
+            return TypedResults.Unauthorized();
+        }
+
+        return TypedResults.Ok(new { Username = user.UserName, Id = user.Id });
+    }
+
+    [Authorize]
+    [Route("feed")]
+    [HttpGet]
+    public IResult GetFeed()
+    {
+        return TypedResults.Ok();
+    }
+}
