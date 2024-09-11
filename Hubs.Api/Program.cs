62dd67a00c4ba917f5fcc1d9c1e85d2370fe6622
luomsa <git@luomsa.com>
2024-09-11 18:56:57 +0300

    add config

--- Hubs.Api/Program.cs
+++ Hubs.Api/Program.cs
@@ -1,12 +1,64 @@
+using System.Text.Json;
+using Hubs.Api;
+using Hubs.Api.Data;
+using Hubs.Api.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers(options =>
+{
+    options.ModelMetadataDetailsProviders.Add(new SystemTextJsonValidationMetadataProvider());
+});
+builder.Services.AddRouting(options =>
+{
+    options.LowercaseUrls = true;
+});
+var connectionString = builder.Configuration["HUB_CONNECTION_STRING"];
+builder.Services.AddDbContext<HubDbContext>(options => { options.UseNpgsql(connectionString); });
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredUniqueChars = 0;
+}).AddEntityFrameworkStores<HubDbContext>();
 
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.Path = "/";
+    options.Events.OnRedirectToLogin = context =>
+    {
+        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        var problemFactory = context.HttpContext.RequestServices.GetRequiredService<ProblemDetailsFactory>();
+        var problem =
+            problemFactory.CreateProblemDetails(context.HttpContext, StatusCodes.Status401Unauthorized, "Unauthorized");
+        return context.Response.WriteAsJsonAsync(problem);
+    };
+    options.Events.OnRedirectToAccessDenied = context =>
+    {
+        context.Response.StatusCode = StatusCodes.Status403Forbidden;
+        var problemFactory = context.HttpContext.RequestServices.GetRequiredService<ProblemDetailsFactory>();
+        var problem =
+            problemFactory.CreateProblemDetails(context.HttpContext, StatusCodes.Status403Forbidden, "Forbidden");
+        return context.Response.WriteAsJsonAsync(problem);
+    };
+
+});
+builder.Services.AddScoped<IHubService, HubService>();
+// builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
-
+app.UseMiddleware<GlobalRoutePrefixMiddleware>("/api");
+app.UsePathBase(new PathString("/api"));
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -15,30 +67,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
