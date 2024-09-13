322636835fef9740e1efc5f77a79ba9035d7000d
luomsa <git@luomsa.com>
2024-09-13 09:02:37 +0300

    check if user is logged in already

--- Hubs.Client/src/context/AuthContext.tsx
+++ Hubs.Client/src/context/AuthContext.tsx
@@ -1,15 +1,17 @@
 ﻿import {
-  createContext,
-  Dispatch,
-  ReactElement,
-  SetStateAction,
-  useContext,
-  useState,
+    createContext,
+    Dispatch,
+    ReactElement,
+    SetStateAction,
+    useContext, useEffect,
+    useState,
 } from "react";
+import client from "../api/http.ts";
 
 const AuthContext = createContext<AuthContext | null>(null);
 type AuthState = {
   username: string;
+  userId: string;
 };
 type AuthContext = {
   user: AuthState | null;
@@ -17,6 +19,18 @@ type AuthContext = {
 };
 const AuthProvider = ({ children }: { children: ReactElement }) => {
   const [user, setUser] = useState<AuthState | null>(null);
+    useEffect(() => {
+        if (user == null) {
+            client.GET("/api/users/me").then(({data}) => {
+                if (data === undefined) {
+                    throw new Error("No data");
+                }
+                setUser({username: data.username, userId: data.userId});
+            }).catch((error) => {
+                console.log(error);
+            });
+        }
+    }, []);
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       {children}
