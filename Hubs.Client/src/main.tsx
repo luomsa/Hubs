720915051dd53ba440dac661664ae054cccac3ec
luomsa <git@luomsa.com>
2024-09-13 09:03:15 +0300

    add routing

--- Hubs.Client/src/main.tsx
+++ Hubs.Client/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import AuthProvider from "./context/AuthContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {RouterProvider} from "react-router-dom";
+import router from "./routes/router.tsx";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
@@ -12,7 +13,7 @@ createRoot(document.getElementById("root")!).render(
     <ChakraProvider>
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
-          <App />
+            <RouterProvider router={router}/>
         </AuthProvider>
       </QueryClientProvider>
     </ChakraProvider>
