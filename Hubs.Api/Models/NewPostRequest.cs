82110d1f52119f660a652feae813f2430e043e34
luomsa <git@luomsa.com>
2024-09-16 17:55:38 +0300

    update validation

--- Hubs.Api/Models/NewPostRequest.cs
+++ Hubs.Api/Models/NewPostRequest.cs
@@ -6,15 +6,17 @@ namespace Hubs.Api.Models;
 public record NewPostRequest
 {
     [Required]
-    [MinLength(1)]
+    [MinLength(3)]
     [MaxLength(100)]
     public string Title { get; set; } = null!;
 
     [Required]
-    [MinLength(1)]
+    [MinLength(3)]
     [MaxLength(1000)]
     public string Content { get; set; } = null!;
     [Required]
+    [MinLength(3)]
+    [MaxLength(20)]
     public string HubName { get; set; } = null!;
     [Required]
     public PostType Type { get; set; }
--- Hubs.Client/src/routes/SubmitPost/SubmitPost.tsx
+++ Hubs.Client/src/routes/SubmitPost/SubmitPost.tsx
@@ -22,22 +22,22 @@ const SubmitPost = () => {
     },
   });
   const onSubmit: SubmitHandler<NewPostRequest> = async (input) => {
-    console.log(input);
     const { data } = await client.POST("/api/posts", {
       body: { ...input, hubName: hub.name! },
     });
-    if (data !== undefined) {
-      // navigate(`/hub/${hub.name}/${data.postId}/${data.slug}`);
+    if (data === undefined) {
+      throw new Error("No data");
     }
+    navigate(`/hub/${hub.name}/${data.postId}/${data.slug}`);
   };
   return (
     <div>
-      <h1>hub/{hub.name}</h1>
       <form
         className={styles.form}
         style={{ width: "100%" }}
         onSubmit={handleSubmit(onSubmit)}
       >
+        <h1>hub/{hub.name}</h1>
         <div className={styles.title}>
           <label htmlFor={"title"}>Title</label>
           <Input
@@ -45,6 +45,14 @@ const SubmitPost = () => {
             type="text"
             {...register("title", {
               required: { value: true, message: "Title is required" },
+              minLength: {
+                value: 3,
+                message: "Title has to be at least 3 characters long",
+              },
+              maxLength: {
+                value: 100,
+                message: "Title has to be at most 100 characters long",
+              },
             })}
           />
           <div>{errors.title?.message}</div>
@@ -79,6 +87,14 @@ const SubmitPost = () => {
               <Textarea
                 {...register("content", {
                   required: { value: true, message: "Body is required" },
+                  minLength: {
+                    value: 3,
+                    message: "Body has to be at least 3 characters long",
+                  },
+                  maxLength: {
+                    value: 1000,
+                    message: "Body has to be at most 1000 characters long",
+                  },
                 })}
               />
             ) : (
@@ -86,6 +102,14 @@ const SubmitPost = () => {
                 type={"text"}
                 {...register("content", {
                   required: { value: true, message: "Link to image missing" },
+                  minLength: {
+                    value: 3,
+                    message: "Body has to be at least 3 characters long",
+                  },
+                  maxLength: {
+                    value: 1000,
+                    message: "Body has to be at most 1000 characters long",
+                  },
                   validate: async (value) => {
                     try {
                       const result = await fetch(value, { method: "head" });
