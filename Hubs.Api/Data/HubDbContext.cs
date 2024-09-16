dcadd05bcc2e81e1ef242c9a9afc471a1e52181b
luomsa <git@luomsa.com>
2024-09-16 09:59:35 +0300

    add add new entities to dbcontext

--- Hubs.Api/Data/HubDbContext.cs
+++ Hubs.Api/Data/HubDbContext.cs
@@ -8,6 +8,10 @@ public class HubDbContext: IdentityDbContext
     public new DbSet<User> Users { get; set; }
     public DbSet<Hub> Hubs { get; set; }
     public DbSet<HubMember> HubMembers { get; set; }
+    
+    public DbSet<Post> Posts { get; set; }
+    
+    public DbSet<Comment> Comments { get; set; }
     public HubDbContext(DbContextOptions options) : base(options)
     {
     }
@@ -17,7 +21,10 @@ public class HubDbContext: IdentityDbContext
         base.OnModelCreating(builder);
         builder.Entity<HubMember>().HasKey(hm => new { User = hm.UserId, Hub = hm.HubId });
         builder.Entity<Hub>().HasMany<HubMember>(h => h.HubMembers).WithOne(hm => hm.Hub);
+        builder.Entity<Hub>().HasMany<Post>(h => h.Posts).WithOne(p => p.Hub);
         builder.Entity<User>().HasMany<HubMember>(u => u.HubMembers).WithOne(hm => hm.User);
+        builder.Entity<User>().HasMany<Post>(u => u.Posts).WithOne(p => p.Author);
+        builder.Entity<User>().HasMany<Comment>(u => u.Comments).WithOne(c => c.Author);
         builder.Entity<Post>().HasMany<Comment>(p => p.Comments).WithOne(c => c.Post);
         builder.Entity<HubMember>().Property(hm => hm.JoinedAt).HasDefaultValueSql("now()");
         builder.Entity<User>().Property(u => u.CreatedAt).HasDefaultValueSql("now()");
@@ -27,5 +34,6 @@ public class HubDbContext: IdentityDbContext
         builder.Entity<Comment>().Property(c => c.CreatedAt).HasDefaultValueSql("now()");
         builder.Entity<User>().Property(u => u.UserName).IsRequired();
         builder.Entity<User>().Property(u => u.NormalizedUserName).IsRequired();
+        builder.Entity<Post>().Property(p => p.Type).HasConversion<string>();
     }
 }
