a87f89262c9a119b6aa579801c4c726a9d3444ca
luomsa <git@luomsa.com>
2024-09-16 17:56:20 +0300

    make compiler happy

--- Hubs.Api/Models/CommentDto.cs
+++ Hubs.Api/Models/CommentDto.cs
@@ -6,10 +6,9 @@ public record CommentDto()
 {
     [Required]
     public int CommentId { get; set; }
-    [Required]
-    public string Content { get; set; }
-    [Required]
-    public string Author { get; set; }
+
+    [Required] public string Content { get; set; } = null!;
+    [Required] public string Author { get; set; } = null!;
     [Required]
     public DateTime CreatedAt { get; set; }
     [Required]
