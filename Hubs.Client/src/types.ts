707bf9d44d6abbad590a8eca789b3a4accbf14b6
luomsa <git@luomsa.com>
2024-09-16 10:04:39 +0300

    extract openapi types into separate file to be reused

---
+++ Hubs.Client/src/types.ts
@@ -0,0 +1,14 @@
+﻿import { components } from "./api/schema";
+
+export type PostType = components["schemas"]["PostType"];
+export type NewPostRequest = components["schemas"]["NewPostRequest"];
+export type PostDto = components["schemas"]["PostDto"];
+export type HubDto = components["schemas"]["HubDto"];
+export type PostDetailType = {
+  post: PostDto;
+  hub: HubDto;
+};
+export type CommentDto = components["schemas"]["CommentDto"];
+export type HubParams = {
+  name: string;
+};
