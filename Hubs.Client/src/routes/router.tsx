6d9b5b916a5d375f6db48203585306d190c3e4a6
luomsa <git@luomsa.com>
2024-09-16 10:03:04 +0300

    add hub routes

--- Hubs.Client/src/routes/router.tsx
+++ Hubs.Client/src/routes/router.tsx
@@ -1,36 +1,72 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App.tsx";
 import Home from "./Home.tsx";
 import client from "../api/http.ts";
-import Hub from "./Hub.tsx";
 import Popular from "./Popular.tsx";
+import SubmitPost from "./SubmitPost/SubmitPost.tsx";
+import Hub from "./Hub/Hub.tsx";
+import Post from "./Post/Post.tsx";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>,
-        children: [{
-            path: "",
-            element: <Home/>,
-        }, {
-            path: "/popular",
-            element: <Popular/>
-        }, {
-            path: "/hub/:name",
-            loader: async ({params}) => {
-                if (params.name == null) {
-                    throw new Error("No hub name provided");
-                }
-                const {data} = await client.GET(`/api/hubs/{name}`, {
-                    params: {
-                        path: {name: params.name}
-                    }
-                });
-                return data;
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/popular",
+        element: <Popular />,
+      },
+      {
+        path: "/hub/:name",
+        loader: async ({ params }) => {
+          if (params.name === undefined) {
+            throw new Error("No hub name provided");
+          }
+          const { data } = await client.GET(`/api/hubs/{name}`, {
+            params: {
+              path: { name: params.name },
             },
-            element: <Hub/>,
-            errorElement: <div>This hub doesn't exist</div>
-        }]
-    }
+          });
+          return data;
+        },
+        element: <Hub />,
+        errorElement: <div>This hub doesn't exist</div>,
+      },
+      {
+        path: "/hub/:name/submit",
+        element: <SubmitPost />,
+        errorElement: <div>Something went wrong</div>,
+      },
+      {
+        path: "/hub/:name/:postId/:slug",
+        loader: async ({ params }) => {
+          if (
+            params.name == null ||
+            params.postId == null ||
+            params.slug == null
+          ) {
+            throw new Error("No parameters provided");
+          }
+          const { data: post } = await client.GET("/api/posts/{postId}", {
+            params: {
+              path: { postId: params.postId },
+            },
+          });
+          const { data: hub } = await client.GET(`/api/hubs/{name}`, {
+            params: {
+              path: { name: params.name },
+            },
+          });
+          return { post, hub };
+        },
+        element: <Post />,
+        errorElement: <div>Something went wrong</div>,
+      },
+    ],
+  },
 ]);
 export default router;
