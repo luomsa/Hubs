911dea7156ffa7a7dc9873c99905fc7a3d88d4bb
luomsa <git@luomsa.com>
2024-10-15 14:32:54 +0300

    add handler to catch thrown custom exceptions

---
+++ Hubs.Api/Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Hubs.Api.Exceptions;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception,
+        CancellationToken cancellationToken)
+    {
+        var problemFactory = httpContext.RequestServices.GetRequiredService<ProblemDetailsFactory>();
+        switch (exception)
+        {
+            case HubExistsException ex:
+            {
+                const int statusCode = StatusCodes.Status409Conflict;
+                var problem = problemFactory.CreateProblemDetails(httpContext, statusCode,
+                    ex.Message);
+                httpContext.Response.StatusCode = statusCode;
+                await httpContext.Response.WriteAsJsonAsync(problem, cancellationToken);
+                return true;
+            }
+            case HubNotFoundException ex:
+            {
+                const int statusCode = StatusCodes.Status404NotFound;
+                var problem = problemFactory.CreateProblemDetails(httpContext, statusCode,
+                    ex.Message);
+                httpContext.Response.StatusCode = statusCode;
+                await httpContext.Response.WriteAsJsonAsync(problem, cancellationToken);
+                return true;
+            }
+            case PostNotFoundException ex:
+            {
+                const int statusCode = StatusCodes.Status404NotFound;
+                var problem = problemFactory.CreateProblemDetails(httpContext, statusCode,
+                    ex.Message);
+                httpContext.Response.StatusCode = statusCode;
+                await httpContext.Response.WriteAsJsonAsync(problem, cancellationToken);
+                return true;
+            }
+            case VoteException ex:
+            {
+                const int statusCode = StatusCodes.Status409Conflict;
+                var problem = problemFactory.CreateProblemDetails(httpContext, statusCode,
+                    ex.Message);
+                httpContext.Response.StatusCode = statusCode;
+                await httpContext.Response.WriteAsJsonAsync(problem, cancellationToken);
+                return true;
+            }
+            default:
+                return false;
+        }
+    }
+}
