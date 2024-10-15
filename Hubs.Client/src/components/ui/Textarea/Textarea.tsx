2123b6db0f10d47167ea9c628c251708e86a8cb4
luomsa <git@luomsa.com>
2024-10-15 14:31:32 +0300

    ui component tweaks

--- Hubs.Client/src/components/ui/Textarea/Textarea.tsx
+++ Hubs.Client/src/components/ui/Textarea/Textarea.tsx
@@ -1,6 +1,21 @@
-﻿import { ForwardedRef, forwardRef } from "react";
+﻿import {
+  ClassAttributes,
+  ForwardedRef,
+  forwardRef,
+  InputHTMLAttributes,
+} from "react";
 import styles from "./Textarea.module.css";
-const Textarea = forwardRef((props, ref: ForwardedRef<any>) => {
-  return <textarea className={styles.textarea} ref={ref} {...props}></textarea>;
-});
+import { JSX } from "react/jsx-runtime";
+const Textarea = forwardRef(
+  (
+    props: JSX.IntrinsicAttributes &
+      ClassAttributes<HTMLTextAreaElement> &
+      InputHTMLAttributes<HTMLTextAreaElement>,
+    ref: ForwardedRef<any>,
+  ) => {
+    return (
+      <textarea className={styles.textarea} ref={ref} {...props}></textarea>
+    );
+  },
+);
 export default Textarea;
