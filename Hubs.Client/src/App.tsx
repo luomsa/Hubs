752662f30b1ce6c47b7a5ab8ae0763f859527875
luomsa <git@luomsa.com>
2024-10-15 14:32:38 +0300

    add mobile sidebar toggle

--- Hubs.Client/src/App.tsx
+++ Hubs.Client/src/App.tsx
@@ -3,9 +3,26 @@ import { Outlet, ScrollRestoration } from "react-router-dom";
 import Sidebar from "./components/Sidebar/Sidebar.tsx";
 import styles from "./App.module.css";
 function App() {
+  const toggleMenu = () => {
+    if (
+      window
+        .getComputedStyle(document.documentElement)
+        .getPropertyValue("--mobile-menu-active") === "block"
+    ) {
+      document.documentElement.style.setProperty(
+        "--mobile-menu-active",
+        "none",
+      );
+    } else {
+      document.documentElement.style.setProperty(
+        "--mobile-menu-active",
+        "block",
+      );
+    }
+  };
   return (
     <>
-      <NavMenu />
+      <NavMenu toggleMenu={toggleMenu} />
       <div className={styles.wrapper}>
         <Sidebar />
         <main className={styles.main}>
